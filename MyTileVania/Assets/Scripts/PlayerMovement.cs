9186c35a4f0328757b4c431ae4bfd760c76aea68
Jmac4Life <143218795+Jmac4Life@users.noreply.github.com>
2023-11-05 02:01:14 -0500

    Horizontal Player Movement (6:21)

--- MyTileVania/Assets/Scripts/PlayerMovement.cs
+++ MyTileVania/Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,20 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float fltRunSpeed = 10f;
     Vector2 moveInput;
+    Rigidbody2D myRigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
     }
 
     void OnMove(InputValue value)
@@ -24,6 +26,13 @@ public class PlayerMovement : MonoBehaviour
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
     }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2 (moveInput.x * fltRunSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+    }
+
 
 
 }
